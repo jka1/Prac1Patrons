e0bcd4be8c59f49401747bfce5faa6b97f4b674b
joelk <joelkinsell@gmail.com>
2019-05-19 22:36:49 +0200

    Created pakage servicelocatortest2

---
+++ src/servicelocatortest2/SimpleServiceLocatorTest.java
@@ -0,0 +1,20 @@
+package servicelocatortest2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleServiceLocatorTest {
+
+    @Test
+    void setService() {
+    }
+
+    @Test
+    void setConstant() {
+    }
+
+    @Test
+    void getObject() {
+    }
+}
